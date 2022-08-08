d9e8088651e98271409967d81f850b408b137b6f
rikrdo <{ID}+{username}@users.noreply.github.com>
2022-08-08 12:27:56 +0200

    Irondome Evolution

--- iron_dome/iron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true; do
-
-evento=$(inotify-hookable -w /home/rikrdo/cybersec/iron_dome/logs/) &&
-
-echo $evento >> logfile.log
-
-done
--- iron_dome/irondome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true; do
-
-evento=$(inotifywait -e modify,create,delete -r /home/rikrdo/cybersec/iron_dome/logs/) &&
-
-echo $evento >> logfile.log
-
-done
