9764814d48b342125c1f52f2dcce3a4cbc55bb18
rikrdo <{ID}+{username}@users.noreply.github.com>
2022-08-25 06:22:38 +0200

    Recovery Starting

--- recovery/recovery.py
+++ recovery/recovery.py
@@ -11,14 +11,16 @@ __email__ = "rikrdo@rikrdo.es"
 __status__ = "Production"
 
 print('''                         
-         ...           ...      
-        (- -)         (- o)
-    ooO--(_)--Ooo-ooO--(_)--Ooo-
+          ...           ...      
+         (- -)         (- o)
+    -ooO--(_)--Ooo-ooO--(_)--Ooo-
     ''')
 
-# import OS module
-import os, datetime, subprocess, wmi, sys, winreg
+import os, datetime, subprocess, wmi, sys, winreg, sqlite3
 from Registry import Registry
+from time import sleep
+from printy import printy
+from printy import inputy
 from winreg import (
   ConnectRegistry,
   OpenKey,
@@ -28,59 +30,140 @@ from winreg import (
   HKEY_LOCAL_MACHINE,
   HKEY_CURRENT_USER
 )
- 
-# get list of recent files
-path = r'C:\Users\vagrant\AppData\Roaming\Microsoft\Windows\Recent'
-# list all the files in the recent folder
-dir_list = os.listdir(path)
 
-print("\n  | RECENT ACCESSED FILES | \n")
+### ACTIONS ###
 
-for files in dir_list:
-    access_time = os.path.getatime(path+'//'+files)
-    last_access = format(datetime.datetime.fromtimestamp(access_time).strftime("%d-%m-%Y - %H:%M:%S"))
-    print (" ",last_access," >>> ", files)
-
-print('')
-
-# # traverse the software list
-# Data = subprocess.check_output(['wmic', 'product', 'get', 'name'])
-# a = str(Data)
-
-# # try block
-# try:
-    
-    # # arrange the string
-    # for i in range(len(a)):
-        # print(a.split("\\r\\r\\n")[6:][i])
-  
-# except IndexError as e:
-    # print("All Done")
-
+# Initializing the wmi constructor
 w = wmi.WMI()
-for p in w.Win32_Product():
-    print (str(p.Caption))
+
+def progress():
+    #animation = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
+    #animation = ['|', '/', '-', '\\']
+    animation = ["■         ","■■        ", "■■■       ", "■■■■      ", "■■■■■     ", "■■■■■■    ", "■■■■■■■   ", "■■■■■■■■  ", "■■■■■■■■■ ", "■■■■■■■■■■"]
+             
+    for a in range(1, len(animation)):
+        printy("  Working... [n]"+animation[a]+"@", end='\r')
+        sleep(0.01)
+        print('                                   ', end='\r')
+
+# Fechas de cambio de ramas de registro (CurrentVersionRun)
+def branch():
+    printy("\n  | [rB]CURRENT VERSION RUN@ | \n")
+    with ConnectRegistry(None, HKEY_LOCAL_MACHINE) as hive:
+        with OpenKey(hive, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion", 0, KEY_ALL_ACCESS) as hosts_key:
+            num_of_values = QueryInfoKey(hosts_key)[1]
+            for i in range(num_of_values):
+                values = EnumValue(hosts_key, i)
+                if (values[0] == "InstallDate"):
+                    dt_install = format(datetime.datetime.fromtimestamp(values[1]).strftime("%d-%m-%Y - %H:%M:%S"))
+                    print('  Installed date: ', dt_install)                
+                    #print("\n\n ", values)
+    inputy("\n  [gI]Press Enter to continue...@")
+    recent_files()
     
+    #Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select-object DisplayName
+    
+# Archivos recientes
+def recent_files():
+    printy("\n  | [rB]RECENT ACCESSED FILES@ | \n")
+    # get list of recent files
+    path = r'C:\Users\vagrant\AppData\Roaming\Microsoft\Windows\Recent'
+    # list all the files in the recent folder
+    dir_list = os.listdir(path)
 
-# with ConnectRegistry(None, HKEY_LOCAL_MACHINE) as hive:
-    # with OpenKey(hive, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion", 0, KEY_ALL_ACCESS) as hosts_key:
-        # num_of_values = QueryInfoKey(hosts_key)[1]
-        # for i in range(num_of_values):
-            # values = EnumValue(hosts_key, i)
-            # print(values)
+    for files in dir_list:
+        progress()
+        access_time = os.path.getatime(path+'//'+files)
+        last_access = format(datetime.datetime.fromtimestamp(access_time).strftime("%d-%m-%Y - %H:%M:%S"))
+        print (" ",last_access," >>> ", files)
 
-print("****************") 
+    inputy("\n  [gI]Press Enter to continue...@")
+    installed_software()
 
-#connecting to key in registry
-access_registry = winreg.ConnectRegistry(None,winreg.HKEY_CURRENT_USER)
+# Programas instalados
+def installed_software():
+    printy("\n  | [rB]INSTALLED SOFTWARE@ | \n")
 
-access_key = winreg.OpenKey(access_registry,r"SOFTWARE\SMicrosoft\Currentversion\Search\RecentApps")
-#accessing the key to open the registry directories under
-for n in range(20):
-   try:
-      x =winreg.EnumKey(access_key,n)
-      print(x)
-   except:
-      break
+    for p in w.Win32_Product():
+        progress()
+        print (" ", str(p.Caption))
+    
+    inputy("\n  [gI]Press Enter to continue...@")
+    open_apps()
 
-#Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select-object DisplayName
+# Programas abiertos
+def open_apps():
+    printy("\n  | [rB]OPEN APPS@ | \n")
+    app_set = set()
+    # Iterating through all the running processes
+    for process in w.Win32_Process():
+        progress()
+        if not process.Name in app_set:
+            print(" " , process.Name)
+        app_set.add(process.Name)
+    
+    inputy("\n  [gI]Press Enter to continue...@")
+    reg_access()
+
+# Historial de navegación
+def browser_history():
+    database_file = r'C:\Users\Vagrant\AppData\Local\Microsoft\Edge\User Data\Default\History'
+    print(database_file)
+    print ("Accessing Google Chrome browsing history.")
+    db = sqlite3.connect(database_file)
+    print(db)
+    cursor = db.cursor()
+    cursor.execute("SELECT * from urls")
+    browsing_data = (cursor.fetchall())
+    for record in browsing_data:
+        visit_time = str(datetime.datetime(1601,1,1) + datetime.timedelta(microseconds=record[5]))
+        if visit_time[:4] == "1601":
+            pass
+        else:
+            visit_time = str(datetime.datetime.strptime(visit_time, "%d-%m-%Y %H:%M:%S"))
+            visit_time = visit_time[:-7]
+    printable = set(string.printable)
+    visit_title = filter (lambda x: x in printable, record[2])
+    visit_title = visit_title.replace(",","")
+    visit_url = record[1]
+    visit_line = visit_time + "," + "Website visited (Chrome)" + "," + "\"" + visit_title + "\"" + "," + "\"" + visit_url + "\"" + "," + username + "," + "," + "," + visit_time + "," + "," + "Google Chrome history" + "," + "History" + "\n"
+
+    print(visit_line)
+
+# Dispositivos conectados
+def connected_devices():
+    pass
+    
+# Eventos de log 
+def log_events():
+    pass
+
+### HAPPY ENDING ###    
+      
+# pretty script end after ctrl + c
+
+def exit_nice():
+    print('\n -------------------------- *** Succesfully ends!!! *** ------------------------- \n')
+
+def kill_key():
+    print('\n -------------------------- *** Keyboard Interrupted!!! *** -------------------------', end="\r")
+    sleep(0.5)
+    print("                                                                                                          ", end="\r")
+
+### MAIN ### 
+
+def main():
+    try:
+        #branch()
+        #open_apps()
+        browser_history()
+    except:
+        print('\n  WTF!')
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        kill_key()
+    finally:
+        exit_nice()
