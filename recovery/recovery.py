48aacedff2ee285b8f77aa4f6389c244c7eea83d
rikrdo <echeverriabarrena@gmail.com>
2022-08-29 14:49:50 +0200

    README.md changes

--- recovery/recovery.py
+++ recovery/recovery.py
@@ -21,7 +21,7 @@ print('''
                  \/     \/                \/       \/     
 ''')
 
-import os, datetime, subprocess, wmi, sys, winreg, sqlite3
+import os, datetime, subprocess, wmi, sys, winreg, sqlite3, win32evtlog
 from datetime import datetime
 from Registry import Registry
 from time import sleep
@@ -46,12 +46,15 @@ def progress():
     #animation = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
     #animation = ['|', '/', '-', '\\']
     animation = ["■         ","■■        ", "■■■       ", "■■■■      ", "■■■■■     ", "■■■■■■    ", "■■■■■■■   ", "■■■■■■■■  ", "■■■■■■■■■ ", "■■■■■■■■■■"]
-             
-    for a in range(1, len(animation)):
-        printy("  Working... [n]"+animation[a]+"@", end='\r')
-        sleep(0.01)
-        print('                                   ', end='\r')
-
+    try:         
+        for a in range(1, len(animation)):
+            printy("  Working... [n]"+animation[a]+"@", end='\r')
+            sleep(0.01)
+            print('                                   ', end='\r')
+    except:
+        kill_nicelly()
+        sys.exit(1)    
+        
 # Fechas de cambio de ramas de registro (CurrentVersionRun)
 def branch():
     printy("\n  | [rB]CURRENT VERSION RUN@ | \n")
@@ -123,16 +126,16 @@ def browser_history():
     browswers_list.append(os.path.expanduser('~')+r'\AppData\Local\Google\Chrome\User Data\Default')
     #firefox = os.path.expanduser('~')+r'\AppData\Local\Google\Chrome\User Data\Default'
     for browser in browswers_list:
-        browser_name = browser.split('\\')[6]
-        printy('\n  | [rB]'+browser_name.upper()+'@ |\n')
-        
         try:
-            files = os.listdir(browser)
-            history_db = os.path.join(browser, 'history')
-            db = sqlite3.connect(history_db)
-            cursor = db.cursor()
-            cursor.execute("SELECT url, title, datetime(urls.last_visit_time/1000000-11644473600, 'unixepoch') last_visit_time from urls")
-            browsing_data = (cursor.fetchall())
+            if (os.path.isdir(browser)):
+                browser_name = browser.split('\\')[6]
+                printy('\n  | [rB]'+browser_name.upper()+'@ |\n')        
+                files = os.listdir(browser)
+                history_db = os.path.join(browser, 'history')
+                db = sqlite3.connect(history_db)
+                cursor = db.cursor()
+                cursor.execute("SELECT url, title, datetime(urls.last_visit_time/1000000-11644473600, 'unixepoch') last_visit_time from urls")
+                browsing_data = (cursor.fetchall())
             
             for record in browsing_data:
                 progress()
@@ -146,15 +149,75 @@ def browser_history():
 
 # Dispositivos conectados
 def connected_devices():
-    pass
+    today = datetime.today()
+    today = format(datetime.today.strftime("%d-%m-%Y - %H:%M:%S"))
+    print("NOW:", today)
+    if w.Win32_PhysicalMedia():
+        for item in w.Win32_PhysicalMedia():
+            if item.Name:
+                print("ITEM: ", item.Name)
+    else:
+        print("No Physical Media devices")
+
+    for drive in w.Win32_DiskDrive():
+        print("DRIVE:", drive.Name)
+
+    for disk in w.Win32_LogicalDisk():
+        print("DISK: ", disk.Name)
+        print("DISK TYPE: ", hd_type(disk.DriveType))
+
+    for usb in w.Win32_USBController():
+        print("USB: ", usb.Name)
+    
+def hd_type(h_type):
+    if(h_type == 0):
+        c_type = "Unknown"
+    elif(h_type == 1):
+        c_type = "No Root Directory"
+    elif(h_type == 2):
+        c_type = "Removable Disk"
+    elif(h_type == 3):
+        c_type = "Local Disk"
+    elif(h_type == 4):
+        c_type = "Network Drive"
+    elif(h_type == 5):
+        c_type = "Compact Disc"
+    elif(h_type == 6):
+        c_type = "RAM Disk"
+    return c_type
+    
+    log_events()
     
 # Eventos de log 
 def log_events():
-    p = subprocess.run("Get-EventLog -List", shell=True ,capture_output=True,text=True)#!!if windows shell=True!!
+    #p = subprocess.run("Get-EventLog -List", shell=True ,capture_output=True,text=True)#!!if windows shell=True!!
+    #p = subprocess.run("Get-WinEvent -ListLog *", shell=True)
+    #print(p)
+    #output = p.stdout
+    #print(output)
 
-    output = p.stdout
-    
-    print(output)
+    server = 'localhost'
+    logtype = 'Application'
+    hand = win32evtlog.OpenEventLog(server,logtype)
+    flags = win32evtlog.EVENTLOG_SEQUENTIAL_READ | win32evtlog.EVENTLOG_BACKWARDS_READ
+    total = win32evtlog.GetNumberOfEventLogRecords(hand)
+    events = win32evtlog.ReadEventLog(hand,flags,0)
+    while True:
+        if events:
+            for event in events:
+                print ('Event Category:', event.EventCategory)
+                print ('Time Generated:', event.TimeGenerated)
+                print ('Source Name:', event.SourceName)
+                print ('Event ID:', event.EventID)
+                print ('Event Type:', event.EventType)
+                data = event.StringInserts
+                if data:
+                    print ('Event Data:')
+                else:
+                    pass
+
+    print ("Total number of Event record ",total)
+    print ("Log record read",len(events))
 
 ### HAPPY ENDING ###    
       
@@ -172,9 +235,10 @@ def kill_nicelly():
 
 def main():
     #try:
-        branch()
+        #branch()
         #open_apps()
         #browser_history()
+    connected_devices()
         #log_events()
     #except:
         #print('\n  WTF!')
